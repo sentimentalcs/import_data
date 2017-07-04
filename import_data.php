ddf1e9baf17de9e3268fb73017b44f653ed63bdc
sentimentalcs <310256023@qq.com>
2017-07-04 20:09:57 +0800

    add

--- import_data.php
+++ import_data.php
@@ -1,1 +1,12 @@
-import_data.php
+<?php
+
+	
+	include './MyPDO.php';
+
+	$pdo = MyPDO::getInstance('localhost','root','','cpic','utf8');
+
+
+	$sql = 'SELECT dept_id,dept_name FROM cpic_dept LIMIT 10';
+	$res = $pdo -> query($sql);
+	var_dump($res);
+	exit;
